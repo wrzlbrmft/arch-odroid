a361b16bacd6f9b665737ead35bd30b4f53c686b
matthias <matthias@calvin>
2016-01-30 22:55:36 +0100

    now ARCH_LINUX_DOWNLOAD_URL

--- arch-xu4.sh
+++ arch-xu4.sh
@@ -79,10 +79,10 @@ doConfirmInstall() {
 }
 
 doDownloadArchLinux() {
-	if [ ! -f "`basename "$ARCH_LINUX_DOWNLOAD"`" ] || [ "$ARCH_LINUX_DOWNLOAD_FORCE" == "yes" ]; then
-		rm -f "`basename "$ARCH_LINUX_DOWNLOAD"`"
+	if [ ! -f "`basename "$ARCH_LINUX_DOWNLOAD_URL"`" ] || [ "$ARCH_LINUX_DOWNLOAD_FORCE" == "yes" ]; then
+		rm -f "`basename "$ARCH_LINUX_DOWNLOAD_URL"`"
 		curl --retry 999 --retry-delay 0 --retry-max-time 300 --speed-time 10 --speed-limit 0 \
-			-LO "$ARCH_LINUX_DOWNLOAD"
+			-LO "$ARCH_LINUX_DOWNLOAD_URL"
 	fi
 }
 
@@ -158,7 +158,7 @@ doMount() {
 }
 
 doUnpackArchLinux() {
-	tar xvf "`basename "$ARCH_LINUX_DOWNLOAD"`" -C root -p
+	tar xvf "`basename "$ARCH_LINUX_DOWNLOAD_URL"`" -C root -p
 }
 
 doFinalizeBoot() {
